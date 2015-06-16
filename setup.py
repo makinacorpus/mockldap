c839ed9bba0b13c93e992a928cd0ea7a3ede4c97
Peter Sagerson <psagers@ignorare.net>
2015-06-16 12:21:28 -0700

    Version 0.2.5

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ else:
 
 setup(
     name='mockldap',
-    version='0.2.4',
+    version='0.2.5',
     description=u"A simple mock implementation of python-ldap.",
     long_description=open('README').read(),
     url='http://bitbucket.org/psagers/mockldap/',
