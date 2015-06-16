4a4156f63f32f0957d51424d5676f417e8614518
Peter Sagerson <psagers@ignorare.net>
2015-06-16 11:42:48 -0700

    Fix for case-insensitive DN matching in search_s.

--- src/mockldap/ldapobject.py
+++ src/mockldap/ldapobject.py
@@ -242,7 +242,7 @@ class LDAPObject(RecordableMethods):
         # Find directory entries within the requested scope
         base_parts = ldap.dn.explode_dn(base.lower())
         base_len = len(base_parts)
-        dn_parts = dict((dn, ldap.dn.explode_dn(dn)) for dn in self.directory.keys())
+        dn_parts = dict((dn, ldap.dn.explode_dn(dn.lower())) for dn in self.directory.keys())
 
         if scope == ldap.SCOPE_BASE:
             dns = (dn for dn, parts in dn_parts.items() if parts == base_parts)
